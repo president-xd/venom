e14a5f59bcd86b892a38b2f3597fa95689727d88
president-xd <lasharimohsin19@gmail.com>
2026-06-02 17:27:56 +0500

    add: venom/flows/__init__.py exporting account takeover and discount abuse flows

--- venom/flows/__init__.py
+++ venom/flows/__init__.py
@@ -3,7 +3,15 @@ Multi-step, possibly cross-host exploitation *flows* that don't fit the
 single-request playbook model (e.g. account lifecycle with email verification).
 """
 
+from .account_privilege import run as account_privilege
 from .account_takeover import run as account_takeover
 from .coupon_stacking import run as coupon_stacking
+from .exceptional_input import run as exceptional_input
+from .infinite_money import run as infinite_money
+from .integer_overflow import run as integer_overflow
+from .login_statemachine import run as login_statemachine
+from .workflow_skip import run as workflow_skip
 
-__all__ = ["account_takeover", "coupon_stacking"]
+__all__ = ["account_privilege", "account_takeover", "coupon_stacking",
+           "exceptional_input", "infinite_money", "integer_overflow",
+           "login_statemachine", "workflow_skip"]

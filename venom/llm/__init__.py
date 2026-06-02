0bbf685e654d50a3ee0e4603fe8caf308ba7dbd5
president-xd <lasharimohsin19@gmail.com>
2026-06-02 13:24:22 +0500

    Add venom/llm/__init__.py exporting multi-provider LLM routers

---
+++ venom/llm/__init__.py
@@ -0,0 +1,9 @@
+from .providers import LLMRouter, ProviderConfig, Provider, TaskType, complete_json
+
+__all__ = [
+    "LLMRouter",
+    "ProviderConfig",
+    "Provider",
+    "TaskType",
+    "complete_json",
+]
